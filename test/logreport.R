104331b8d93407f211c4a665a9812ff91a89ae4c
Asli Gurer <agurer@maelstrom-research.org>
2020-01-08 20:06:38 +0100

    Harmonized_dataset_new is deleted from the paths

--- test/logreport.R
+++ test/logreport.R
@@ -1,7 +1,7 @@
-file.remove("Harmonized_dataset_new/test/test.log")
-file.create("Harmonized_dataset_new/test/test.log")
+file.remove("test/test.log")
+file.create("test/test.log")
 
-con <- file("Harmonized_dataset_new/test/test.log")
+con <- file("test/test.log")
 sink(con, append=TRUE)
 sink(con, append=TRUE, type="message")
 
@@ -9,4 +9,4 @@ sink(con, append=TRUE, type="message")
 source("/home/agurer/.Rprofile", echo=TRUE, max.deparse.length=10000)
 
 # And look at the log...
-file.show("Harmonized_dataset_new/test/test.log")
+file.show("test/test.log")
