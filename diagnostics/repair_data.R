7a870383a18a9c41f62a55d3d2bb5b85a4c23607
Asli Gurer <agurer@maelstrom-research.org>
2020-01-17 22:34:21 +0100

    clsa uncommented

--- diagnostics/repair_data.R
+++ diagnostics/repair_data.R
@@ -2,7 +2,9 @@
 # Fixing the variable classes by
 # Changing classes of the variables into the Value Types in the data dictionary, in case they are different in the dataset itself
 
-# try({clsa_total    <- change_class(clsa_total, dd_clasa, "CLSA")})
+#try({rotterdam_total <- change_class(clsa_total, dd_clasa, "ROTTERDAM")})
+try({clsa_cop_total  <- change_class(clsa_cop_total, dd_clasa, "CLSA_COP")})
+try({clsa_tra_total  <- change_class(clsa_tra_total, dd_clasa, "CLSA_TRA")})
 try({globe_total     <- change_class(globe_total,dd_globe, "GLOBE")})
 try({hapiee_lt_total <- change_class(hapiee_lt_total,dd_hapiee_lt, "HAPIEE_LT")})
 try({hapiee_cz_total <- change_class(hapiee_cz_total,dd_hapiee_cz, "HAPIEE_CZ")})
@@ -14,7 +16,9 @@ try({hunt_total      <- change_class(hunt_total,dd_hunt, "HUNT")})
 try({record_total    <- change_class(record_total,dd_record, "RECORD")})
 
 # Excluding unwanted physenv variable from the harmonized dataset of each study
-
+#try({rotterdam_total %<>% select(-starts_with("physenv_cn_bf_facil3000_"))})
+try({clsa_cop_total %<>% select(-starts_with("physenv_cn_bf_facil3000_"))})
+try({clsa_tra_total %<>% select(-starts_with("physenv_cn_bf_facil3000_"))})
 try({globe_total %<>% select(-starts_with("physenv_cn_bf_facil3000_"))})
 try({hapiee_cz_total %<>% select(-starts_with("physenv_cn_bf_facil3000_"))})
 try({hapiee_lt_total %<>% select(-starts_with("physenv_cn_bf_facil3000_"))})
@@ -26,7 +30,9 @@ try({lucas_total %<>% select(-starts_with("physenv_cn_bf_facil3000_"))})
 try({record_total %<>% select(-starts_with("physenv_cn_bf_facil3000_"))})
 
 # Excluding unwanted physenv variable from the data dictionary of each study
-
+#try({dd_rotterdam$Variables %<>% filter(!str_detect(name,"physenv_cn_bf_facil3000_"))})
+try({dd_clsa_cop$Variables %<>% filter(!str_detect(name,"physenv_cn_bf_facil3000_"))})
+try({dd_clsa_tra$Variables %<>% filter(!str_detect(name,"physenv_cn_bf_facil3000_"))})
 try({dd_globe$Variables %<>% filter(!str_detect(name,"physenv_cn_bf_facil3000_"))})
 try({dd_hapiee_cz$Variables %<>% filter(!str_detect(name,"physenv_cn_bf_facil3000_"))})
 try({dd_hapiee_lt$Variables %<>% filter(!str_detect(name,"physenv_cn_bf_facil3000_"))})
