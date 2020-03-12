1a5855dba1ef9e997b44d0d8ef9c5bbf057ef8d6
Asli Gurer <agurer@maelstrom-research.org>
2020-03-12 15:41:31 +0100

    clsa, correct dd's applied

--- export/export_data.R
+++ export/export_data.R
@@ -12,8 +12,8 @@ last_release <- total_release[length(total_release)]
 library(harmor)
 # Applying data dictionaries to each harmonized dataset using harmor function
 #try({rotterdam_total <- applyDictionary(rotterdam_total,     variables=dd_globe$Variables,     categories=dd_globe$Categories)})
-try({clsa_cop_total  <- applyDictionary(clsa_cop_total,   variables=dd_globe$Variables,   categories=dd_globe$Categories)})
-try({clsa_tra_total  <- applyDictionary(clsa_tra_total,   variables=dd_globe$Variables,   categories=dd_globe$Categories)})
+try({clsa_cop_total  <- applyDictionary(clsa_cop_total,   variables=dd_clsa_cop$Variables,   categories=dd_clsa_cop$Categories)})
+try({clsa_tra_total  <- applyDictionary(clsa_tra_total,   variables=dd_clsa_tra$Variables,   categories=dd_clsa_tra$Categories)})
 try({globe_total     <- applyDictionary(globe_total,     variables=dd_globe$Variables,     categories=dd_globe$Categories)})
 try({hapiee_cz_total <- applyDictionary(hapiee_cz_total, variables=dd_hapiee_cz$Variables, categories=dd_hapiee_cz$Categories)})
 try({hapiee_lt_total <- applyDictionary(hapiee_lt_total, variables=dd_hapiee_lt$Variables, categories=dd_hapiee_lt$Categories)})
